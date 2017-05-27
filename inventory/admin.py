2c9a75eccbad1446e2de4567c0ddeca644f65189
kamalogudah <mcpaul2058@gmail.com>
2017-05-27 10:22:11 +0300

    Added Item Model and Changed the Django Admin to Display Item

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item 
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount']
+
+admin.site.register(Item, ItemAdmin)
