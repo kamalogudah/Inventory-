073ec6e0aef1e5843b85f0e3b57261e1a5353c4b
kamalogudah <mcpaul2058@gmail.com>
2017-05-27 11:29:34 +0300

    Configuring Url Patterns

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render 
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<p>In Index view</p>')
+
+def item_detail(request, id):
+    return HttpResponse('<p>In item_detail view with id {0}</p>'.format(id))
